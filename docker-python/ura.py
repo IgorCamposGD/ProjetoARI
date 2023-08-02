4b9b0c8fd35071eb32a471cfefe6823049042264
igor <igorcampos064@gmail.com>
2023-08-02 16:57:03 -0300

    Revert "Compose criado subindo os 2 serviços, asterisk e python"
    This reverts commit 02eefba02a97d37b56841b4477d87aeee79c6ca1.

--- docker-python/ura.py
+++ docker-python/ura.py
@@ -1,42 +1,27 @@
 import os
-import asyncio
-import asyncari
+import ari
+import time
 from variaveisAmbientes import *
 
-async def on_channel_event(channel_obj, event_name):
+def on_channel_event(channel_obj, event_name):
     if event_name == 'StasisStart':
-        playback = await channel_obj.play(media='sound:custom/ura-menu')
-        await asyncio.sleep(8)  # Wait 8 seconds for user input
-
+        playback = channel_obj.play(media='sound:custom/ura-menu')
+        time.sleep(8)  # Wait 8 seconds for user input
         if playback.state != 'done':
-            await playback.stop()
+            playback.stop()
 
         if channel_obj.dtmf_received:
             dtmf_digit = channel_obj.dtmf_received.get('digit')
             if dtmf_digit == '1':
-                await channel_obj.continue_in_dialplan(context='default', extension='200')
+                channel_obj.continue_in_dialplan(context='default', extension='200')
             elif dtmf_digit == '2':
-                await channel_obj.continue_in_dialplan(context='default', extension='201')
+                channel_obj.continue_in_dialplan(context='default', extension='201')
             else:
-                await channel_obj.play(media='sound:custom/invalid-option')
-
+                channel_obj.play(media='sound:custom/invalid-option')
     elif event_name == 'StasisEnd':
-        await channel_obj.play(media='sound:custom/goodbye')
-        await channel_obj.hangup()
+        channel_obj.play(media='sound:custom/goodbye')
+        channel_obj.hangup()
 
-async def main():
-    async with asyncari.connect(os.getenv('ASTERISK_URL'), os.getenv('ASTERISK_APP'), os.getenv('ASTERISK_USER'), os.getenv('ASTERISK_PASS')) as client:
-        client.on_channel_event('StasisStart', on_channel_event)
-        async for m in client:
-            print("** EVENT **", m)
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-
-    # Executar a função para estabelecer a conexão e executar o aplicativo, aguardando eventos indefinidamente
-    try:
-        loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        loop.close()
+client = ari.connect(os.getenv('ASTERISK_URL'), os.getenv('ASTERISK_USER'), os.getenv('ASTERISK_PASS'))
+client.on_channel_event('StasisStart', on_channel_event)
+client.run(apps='ura')
--- docker-python/variaveisAmbientes.py
+++ docker-python/variaveisAmbientes.py
@@ -1,6 +1,5 @@
 import os
 
-os.environ['ASTERISK_URL'] = 'http://172.17.0.2:8088'
+os.environ['ASTERISK_URL'] = 'http://localhost:8088'
 os.environ['ASTERISK_USER'] = 'asterisk'
 os.environ['ASTERISK_PASS'] = 'asterisk'
-os.environ['ASTERISK_APP'] = 'ura'
